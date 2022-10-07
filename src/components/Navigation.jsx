04be908ba45cc3795e2d27464f6346e13bb5fafc
Sabbirhossain94 <sabbirshawon1994@hotmail.com>
2022-10-07 18:33:38 +0600

    user menu updated

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -88,8 +88,6 @@ export default function Navigation({session}) {
                 <Link to="/signin" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Sign In</Link>
                 <Link to="/account" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">User Profile</Link>
                
-
-
               </div>
             </div>
             <div className="flex items-center">
@@ -105,7 +103,7 @@ export default function Navigation({session}) {
                   {showDropDown ? <div onClick={(e) => e.stopPropagation()} className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1">
 
 
-                    <a onClick={()=>{setShowUpdateProfileModal(true);setShowDropDown(false)}} href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-1">Profile</a>
+                    <Link onClick={()=>{setShowUpdateProfileModal(true);setShowDropDown(false)}} to="/account" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-1">Update Profile</Link>
                     <Link to="/createblog" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-2">Create Blog</Link>
                     <a href="#" onClick={logOut} className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-3">Sign out</a>
                   </div> : ''}
