2da412495f99b89c2960f656bfe690f09869cc48
Sabbirhossain97 <sabbirhossainbd199@gmail.com>
2025-01-03 00:43:15 +0600

    dynamic base url added

--- src/services/auth/resetPass.js
+++ src/services/auth/resetPass.js
@@ -1,11 +1,11 @@
 import supabase from "../global/supabaseClient";
 import toast from "react-hot-toast";
+import { baseUrl } from "../../helpers/url";
 
 export const resetPass = async (email) => {
+    const redirectTo = `${baseUrl}/resetpassword`;
     try {
-        const { error } = await supabase.auth.resetPasswordForEmail(email, {
-            redirectTo: `${window.location.origin}/resetpassword`,
-        });
+        const { error } = await supabase.auth.resetPasswordForEmail(email, { redirectTo });
         if (error) throw error;
         toast.success("Password reset link sent to your email");
     } catch (error) {
